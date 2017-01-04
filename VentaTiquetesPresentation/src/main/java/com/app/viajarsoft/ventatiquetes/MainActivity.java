48c07c536cacb0992ca85e9dafeaf20ffa7cf662
Dieber Roa <Dieber Roa>
2017-01-03 21:08:25 -0500

    Se agrega : icono, titulo, llave para build firmado, api key test fairy

--- VentaTiquetesPresentation/src/main/java/com/app/viajarsoft/ventatiquetes/MainActivity.java
+++ VentaTiquetesPresentation/src/main/java/com/app/viajarsoft/ventatiquetes/MainActivity.java
@@ -2,6 +2,7 @@ package com.app.viajarsoft.ventatiquetes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.testfairy.TestFairy;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TestFairy.begin(this, "b16175d0d65f677c363d4bdb11a477aba154070c");
     }
 }
