696844cbbb49e3cf8f4a6c85b4f45b8ab3cc0b78
Jose Tabares <josetaba94@gmail.com>
2017-02-16 14:50:51 -0500

    Despliegue a producción

--- VentaTiquetesPresentation/src/main/java/com/app/viajarsoft/ventatiquetes/MainActivity.java
+++ VentaTiquetesPresentation/src/main/java/com/app/viajarsoft/ventatiquetes/MainActivity.java
@@ -2,7 +2,6 @@ package com.app.viajarsoft.ventatiquetes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.testfairy.TestFairy;
 
 public class MainActivity extends AppCompatActivity {
 
--- VentaTiquetesPresentation/src/main/java/com/app/viajarsoft/ventatiquetes/view/activities/BaseActivity.java
+++ VentaTiquetesPresentation/src/main/java/com/app/viajarsoft/ventatiquetes/view/activities/BaseActivity.java
@@ -14,7 +14,6 @@ import com.app.viajarsoft.ventatiquetes.utilities.helpers.ValidateInternet;
 import com.app.viajarsoft.ventatiquetes.presenters.BasePresenter;
 import com.app.viajarsoft.ventatiquetes.utilities.utils.IConstants;
 import com.app.viajarsoft.ventatiquetes.view.views_activities.IBaseView;
-import com.testfairy.TestFairy;
 
 
 /**
@@ -33,7 +32,6 @@ public class BaseActivity<T extends BasePresenter> extends AppCompatActivity imp
         super.onCreate(savedInstanceState);
         this.validateInternet = new ValidateInternet(this);
         this.customAlertDialog = new CustomAlertDialog(this);
-        TestFairy.begin(this, "9cfa7e8493314cae6d7d6265cff0399be9cb4b15");
     }
 
     @Override
